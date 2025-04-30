50c00efd0d9cf8d4a5bac56db2aaabcc0c4a3bcd
NAGA PRAVEENA KUMARI <praveenakotakonda@gmail.com>
2025-04-30 12:34:33 +0530

    Update mux2.v
    In  Verilog  models

--- mux2.v
+++ mux2.v
@@ -1,7 +1,7 @@
-// module mux2(input a, b, sel,output y);
-// assign y = sel ? b : a;
-// endmodule
-
+// BEHAVIOURAL MODELING
+module mux2(input a, b, sel,output y);
+assign y = sel ? b : a;
+endmodule
 
 module mux2(input a, b, sel,output reg y);
 always@(*) begin 
@@ -11,3 +11,25 @@ if (sel==1'b0)
  y=b;
 end
 endmodule
+
+module mux2(input a, b, sel,output reg y);
+always@(*) begin  
+y = sel ? b : a;
+end
+endmodule
+
+//gate level modeling
+module mux2x1_structural (input wire a, b, sel,output  y);
+    wire not_sel, and_a, and_b;
+    not (not_sel, sel);
+    and (and_a, a, not_sel);
+    and (and_b, b, sel);
+    or (y, and_a, and_b);
+endmodule
+
+
+//data-level
+ module mux2(input a, b, sel,output y);
+assign y = (~sel & b ) |(sel & a);
+ endmodule
+
