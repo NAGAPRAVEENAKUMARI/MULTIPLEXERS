202e6dce08f13ed13ff81942eff1fc8319ca02cb
NAGA PRAVEENA KUMARI <praveenakotakonda@gmail.com>
2025-05-07 06:27:31 +0530

    Update mux4_tb.v

--- mux4_tb.v
+++ mux4_tb.v
@@ -1,43 +1,47 @@
-// `include "mux4.v"
-// `timescale 1ns/1ps
+`include "mux4.v"
+`timescale 1ns/1ps
 
-// module mux4_tb;
-//     reg [3:0] a;
-//     reg [1:0] s;
-//     wire y;
-//  mux4 uut (a, s, y);
-// initial begin
-// $monitor(" s = %b%b, a = %b, y = %b", s[1], s[0], a, y);
+module mux4_tb;
+    reg [3:0] a;
+    reg [1:0] s;
+    wire y;
+ mux4 uut (a, s, y);
+initial begin
+$monitor(" s = %b%b, a = %b, y = %b", s[1], s[0], a, y);
 
-// a = 4'b0000;
-// s = 2'b00;
-// #10 s = 2'b00;
-// #10 s = 2'b01; 
-// #10 s = 2'b10; 
-// #10 s = 2'b11; 
-// #10 a = 4'b1100;
-// #10 s = 2'b00;
-// #10 s = 2'b01; 
-// #10 s = 2'b10; 
-// #10 s = 2'b11; 
+a = 4'b0000;
+s = 2'b00;
+#10 s = 2'b00;
+#10 s = 2'b01; 
+#10 s = 2'b10; 
+#10 s = 2'b11; 
+    
+#10 a = 4'b1100;
+    
+#10 s = 2'b00;
+#10 s = 2'b01; 
+#10 s = 2'b10; 
+#10 s = 2'b11; 
 
-// #10 a = 4'b1111;
-// #10 s = 2'b00;
-// #10 s = 2'b01;
-// #10 s = 2'b10;
-// #10 s = 2'b11;
+#10 a = 4'b1111;
+    
+#10 s = 2'b00;
+#10 s = 2'b01;
+#10 s = 2'b10;
+#10 s = 2'b11;
 
-// #10 $finish;
-// end
+#10 $finish;
+end
 
-// initial begin
-// $dumpfile("mux4_tb.vcd");
-// $dumpvars(0, mux4_tb);
-// end
-// endmodule
+initial begin
+$dumpfile("mux4_tb.vcd");
+$dumpvars(0, mux4_tb);
+end
+endmodule
 
 
-///
+// USING RANDOM TEST-CASES 
+
 `include "mux4.v"
 `timescale 1ns/1ps
 
